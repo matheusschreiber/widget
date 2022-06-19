d27ababe90191765e01a941de31a59b816225346
Matheus <mt.schreiber01@gmail.com>
2022-06-19 13:50:59 -0300

    :wrench: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,10 +5,10 @@ import cors from "cors"
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use(cors())
 
 app.listen(process.env.PORT || 3333, () => {
-    console.log("Listening on port " + process.env.PORT)
+    console.log("Listening on port " + process.env.PORT || 3333)
 })
